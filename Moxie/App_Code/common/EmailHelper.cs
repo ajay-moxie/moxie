eac61a60ae342ce8d4b54136634a4ff1c6f4e18d
Ajay <ajay@moxiedevices.com>
2012-09-22 16:45:49 +0530

    EmailHelper: Function to send email
    Added function to send email.
    Signed-off-by: Ajay <ajay@moxiedevices.com>

---
+++ Moxie/App_Code/common/EmailHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace Moxie.Common
+{
+    public static class EmailHelper
+    {
+        /// <summary>
+        /// Sends email using System.Net.Mail
+        /// </summary>
+        /// <param name="msg">Populated MailMessage object</param>
+        /// <returns>Returns 0 if processed successfully. Any other values indicate failure.</returns>
+
+        public static int SendEmail(string ToEmail, string FromEmail, string Subject, string emailbody)
+        {
+
+            int Err = 0;
+
+            //Only deliver email if both fields are provided.
+            if (!string.IsNullOrEmpty(ToEmail) && !string.IsNullOrEmpty(FromEmail))
+            {
+                try
+                {
+                    MailAddress from = new MailAddress(FromEmail);
+                    MailAddress to = new MailAddress(ToEmail);
+                    MailMessage msg = new MailMessage(from, to);
+
+                    msg.Subject = Subject;
+                    msg.Body = emailbody;
+                    msg.IsBodyHtml = true;
+                    msg.Priority = MailPriority.High;
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.live.com";
+                    smtp.Port = 25;
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new System.Net.NetworkCredential("contact@moxiedevices.com", "nj123456");
+                    smtp.Send(msg);
+                }
+                catch (Exception x)
+                {
+                    Err = 1;
+                    throw new SystemException();
+                }
+            }
+            return Err;
+            
+        }
+    }
+}
