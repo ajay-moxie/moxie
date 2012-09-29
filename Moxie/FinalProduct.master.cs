dccc80253ef12e8a37db9ba411c524f357f54496
Ajay <ajay@moxiedevices.com>
2012-09-29 13:00:54 +0530

    Master: Final Product: First Draft
    Created First Draft for final product
    
    Signed-off-by: Ajay <ajay@moxiedevices.com>

---
+++ Moxie/FinalProduct.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Moxie
+{
+    public partial class FinalProduct : System.Web.UI.MasterPage
+    {
+        public string DownloadLink = "#";
+        public string ProductImageLink = "~/images/no-image-available.jpg";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSheetLink.Text = "Download Detailed Datasheet";
+            DataSheetLink.NavigateUrl = DownloadLink;
+            Img1.Src = ProductImageLink;
+        }
+    }
+}
