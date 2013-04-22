2f12109c858b7ee29416a4738124d337503b1aef
Ajay <ajay@moxiedevices.com>
2013-04-22 19:51:37 +0530

    changed image
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- Moxie/products/Custom-Solutions-and-Consultancy/Default.aspx.cs
+++ Moxie/products/Custom-Solutions-and-Consultancy/Default.aspx.cs
@@ -12,6 +12,7 @@ namespace Moxie.products.Custom_Solutions_and_Consultancy
         protected void Page_Load(object sender, EventArgs e)
         {
             Master.CID = "_4_";
+            Master.inner_image_source = "~/images/custom-solution.png";
         }
     }
 }
