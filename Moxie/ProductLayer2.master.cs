68e8e0ad02bd62439c53cc2d62b71aa212226e3a
Ajay <ajay@moxiedevices.com>
2013-04-25 08:59:47 +0530

    added support of driver type
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- Moxie/ProductLayer2.master.cs
+++ Moxie/ProductLayer2.master.cs
@@ -16,6 +16,7 @@ namespace Moxie
     {
         public string CID = "_0_";
         public string inner_image_source = "~/images/inner101.png";
+        string driver_type = "NULL";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -24,14 +25,31 @@ namespace Moxie
             {
                 try
                 {
+                    driver_type = Request.QueryString["driver"];
                     XmlDocument doc = new XmlDocument();
                     doc.Load(Server.MapPath(ConstantVar.ProductXML));
-                    XmlNodeList textnodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "0" + "')]");
+                    XmlNodeList textnodes;
+                    if (driver_type == "NULL")
+                    {
+                        textnodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "0" + "')]");
+                    }
+                    else
+                    {
+                        textnodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "0" + "') and contains(@driver,'" + driver_type + "')]");
+                    }
 
                     text.DataSource = textnodes;
                     text.DataBind();
-                    XmlNodeList nodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "1" + "')]");
-                    
+                    XmlNodeList nodes;
+                    if (driver_type == "NULL")
+                    {
+                        nodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "1" + "')]");
+                    }
+                    else
+                    {
+                        nodes = doc.SelectNodes("/HEADER/PRODUCT[contains(@CATEGORY,'" + CID + "') and contains(@type,'" + "1" + "') and contains(@driver,'" + driver_type + "')]");
+                    }
+
                     product.DataSource = nodes;
                     product.DataBind();
                     inner_banner1.imgsrc = inner_image_source;
