74a1a91ed5b9d1d8499080d10f631d745e11a169
Ajay <ajay@moxiedevices.com>
2013-04-26 19:22:02 +0530

    led bulb 7w
    Signed-off-by: Ajay <ajay@moxiedevices.com>

---
+++ Moxie/products/led-bulb/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace Moxie.products.led_bulb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string wattage = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            wattage = Request.QueryString["watts"];
+            Page.Header.DataBind();
+            Page.Header.Title = "LED Bulb: " + wattage + "watts" ;
+            HtmlMeta metaTag = new HtmlMeta();
+            metaTag.Name = "Keywords";
+            metaTag.Content = "LED Bulb, " + wattage + "watts";
+            this.Header.Controls.Add(metaTag);
+            Master.PName = "led-bulb-" + wattage + "w";
+        }
+    }
+}
